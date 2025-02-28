411b2be1be3ef036ccf4d6f31889633ec38ef2ef
salihkarakus <sskarakussalih77@gmail.com>
2025-02-28 23:28:57 +0300

    score text added and splash done it

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     public Rigidbody rb;
+    public GameObject splashPrefab;
     public float JumpForce;
     void Update()
     {
@@ -13,6 +14,8 @@ public class Ball : MonoBehaviour
 
     private void OnCollisionEnter(Collision other)
     {
+        GameObject splash = Instantiate(splashPrefab , transform.position + new Vector3(0f , -0.24f , 0f) , transform.rotation);
+        splash.transform.SetParent(other.gameObject.transform);
         rb.AddForce(Vector3.up * JumpForce);
 
         string MaterialName = other.gameObject.GetComponent<MeshRenderer>().material.name;
