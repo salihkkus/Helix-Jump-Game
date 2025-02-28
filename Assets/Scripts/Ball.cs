a7af77da800605ab83fd784a5d300c216f90db17
salihkarakus <sskarakussalih77@gmail.com>
2025-03-01 00:49:16 +0300

    ball script updated

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -7,26 +7,31 @@ public class Ball : MonoBehaviour
     public Rigidbody rb;
     private GameManager gm;
     public GameObject splashPrefab;
-    public float JumpForce;
-    void Update()
+    public float JumpForce = 10f; // Default değer
+
+    void Start()
     {
-        gm = GameManager.FindObjectOfType<GameManager>();
+        gm = FindObjectOfType<GameManager>();
     }
 
     private void OnCollisionEnter(Collision other)
     {
-        GameObject splash = Instantiate(splashPrefab , transform.position + new Vector3(0f , -0.24f , 0f) , transform.rotation);
+        // Splash efektini oluştur ve parent olarak çarptığı objeyi belirle
+        GameObject splash = Instantiate(splashPrefab, transform.position + new Vector3(0f, -0.24f, 0f), transform.rotation);
         splash.transform.SetParent(other.gameObject.transform);
-        rb.AddForce(Vector3.up * JumpForce);
 
+        // Topun yukarı yönlü hızını sıfırlayıp, yeni velocity belirleme
+        rb.velocity = new Vector3(rb.velocity.x, JumpForce, rb.velocity.z);
+
+        // Çarpılan yüzeyin materyal adını kontrol et
         string MaterialName = other.gameObject.GetComponent<MeshRenderer>().material.name;
         Debug.Log(MaterialName);
 
-        if(MaterialName == "UnsafeColor (Instance)")
+        if (MaterialName == "UnsafeColor (Instance)")
         {
             gm.RestartGame();
         }
-        else if(MaterialName == "Last Ring (Instance)")
+        else if (MaterialName == "Last Ring (Instance)")
         {
             Debug.Log("Next Level");
         }
--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -13,7 +13,7 @@ public class CameraController : MonoBehaviour
     }
 
     
-    void Update()
+    void FixedUpdate()
     {
         Vector3 newPos = Vector3.Lerp(transform.position , offset + ball.position , smoothSpeed);
         transform.position = newPos;
