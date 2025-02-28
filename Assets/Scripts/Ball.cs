535ffc5bc4c405b053a15fa6f7ee912416313b68
salihkarakus <sskarakussalih77@gmail.com>
2025-03-01 00:40:45 +0300

    script and cam updated

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     public Rigidbody rb;
+    private GameManager gm;
     public GameObject splashPrefab;
     public float JumpForce;
     void Update()
     {
-        
+        gm = GameManager.FindObjectOfType<GameManager>();
     }
 
     private void OnCollisionEnter(Collision other)
@@ -21,18 +22,13 @@ public class Ball : MonoBehaviour
         string MaterialName = other.gameObject.GetComponent<MeshRenderer>().material.name;
         Debug.Log(MaterialName);
 
-        if(MaterialName == "SafeColor (Instance)")
+        if(MaterialName == "UnsafeColor (Instance)")
         {
-            //get points
-        }
-        else if(MaterialName == "UnsafeColor (Instance)")
-        {
-            //restart game
-            Debug.Log("game over");
+            gm.RestartGame();
         }
         else if(MaterialName == "Last Ring (Instance)")
         {
-            //skip next level
+            Debug.Log("Next Level");
         }
     }
 }
